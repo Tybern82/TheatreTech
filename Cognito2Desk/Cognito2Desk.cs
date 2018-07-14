46d4c3fcd9c135b1bea05653ace17d4a00ce878f
Jeff <jeffpublic@kintoshmalae.com>
2018-07-14 14:27:45 +1000

    Initial Commit

---
+++ Cognito2Desk/Cognito2Desk.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MinimalisticTelnet;
+
+namespace Cognito2Desk {
+    public class Cognito2Desk {
+        string PROMPT = "API>";
+
+        public string INTENSITY = "Intensity";
+        public string RED_CYC = "Color A";
+        public string GREEN_CYC = "Color B";
+        public string BLUE_CYC = "Color C";
+
+        private TelnetConnection deskConnection;
+
+        public bool IsConnected {
+            get {
+                return deskConnection.IsConnected;
+            }
+        }
+
+        public Cognito2Desk(string host) {
+            deskConnection = new TelnetConnection(host, 11123);
+            if (!deskConnection.IsConnected) throw new InvalidOperationException("Unable to make initial connection to desk at [" + host + "]");
+        }
+
+        public bool AttributeFade(uint fixture, uint percent) {
+            return AttributeFade(fixture, percent, 0);
+        }
+
+        public bool AttributeFade(uint fixture, uint percent, double fadeTime) {
+            return AttributeFade(fixture, INTENSITY, percent, fadeTime);
+        }
+
+        public bool AttributeFade(uint fixture, string channel, uint percent) {
+            return AttributeFade(fixture, channel, percent, 0);
+        }
+
+        public bool AttributeFade(uint fixture, string channel, uint percent, double fadeTime) {
+            if (IsConnected) {
+                deskConnection.WriteLine("API.AttributeFade(" + fixture + ",'" + channel + "'," + percent + "," + fadeTime .ToString("0.00") + ")");
+                string result = deskConnection.Read();
+                if (result.Equals(PROMPT)) return true;
+                else {
+                    // TODO: Log Error
+                }
+            } else {
+                // TODO: Log connection error
+            }
+            return false;
+        }
+
+        public bool AttributeFadeCapture(uint fixture, uint percent) {
+            return AttributeFadeCapture(fixture, percent, 0);
+        }
+
+        public bool AttributeFadeCapture(uint fixture, uint percent, double fadeTime) {
+            return AttributeFadeCapture(fixture, INTENSITY, percent, fadeTime);
+        }
+
+        public bool AttributeFadeCapture(uint fixture, string channel, uint percent) {
+            return AttributeFadeCapture(fixture, channel, percent, 0);
+        }
+
+        public bool AttributeFadeCapture(uint fixture, string channel, uint percent, double fadeTime) {
+            if (IsConnected) {
+                deskConnection.WriteLine("API.AttributeFadeCapture(" + fixture + ",'" + channel + "'," + percent + "," + fadeTime.ToString("0.00") + ")");
+                string result = deskConnection.Read();
+                if (result.Equals(PROMPT)) return true;
+            } else {
+                // TODO: Log connection error
+            }
+            return false;
+        }
+    }
+}
---
+++ FXPlayer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FXPlayer {
+    static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            Cognito2Desk.Cognito2Desk desk = new Cognito2Desk.Cognito2Desk("localhost");
+
+            desk.AttributeFade(1, 50);
+            desk.AttributeFade(2, 25, 24.5);
+            desk.AttributeFade(24, desk.RED_CYC, 100, 20);
+            desk.AttributeFade(24, desk.BLUE_CYC, 0);
+            desk.AttributeFade(24, desk.GREEN_CYC, 0);
+            desk.AttributeFade(24, 100, 30);
+
+            /*
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainWindow());
+            */
+        }
+    }
+}
